f309a73f35bf207d904a2b67b1a288df08fd8d4d
gal evgi <gal756@gmail.com>
2023-10-05 19:05:50 +0300

    main change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO,emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
