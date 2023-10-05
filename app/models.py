c2b7c65a96c1326883f5b3ac5e11425a0f9ab190
gal evgi <gal756@gmail.com>
2023-10-05 16:16:02 +0300

    commit

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class CodeBlock(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(500))
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
+@app.route('/choose_block')
+def code_block():
+    return render_template('choose_block.html')
+
+@app.route('/edit_block')
+def edit_code():
+    return render_template('edit_block.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=5000)
